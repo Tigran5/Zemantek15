eea942f2f994b01a31e6235fe36a8391fdcf44bf
Tigran5 <tikotorg@gmail.com>
2013-07-15 13:56:41 +0400

    new

--- xnir/animal_king.php
+++ xnir/animal_king.php
@@ -4,11 +4,12 @@
 
 // interface of herib
 interface herbivorous{
-    function eat_tree() { }
+    
+    function eat_tree () { }
     function slip(){ }
     function move(){ }
     function hear(){ }
-    
+    function wight(){ }
     
 }
 // interface spredator
@@ -23,29 +24,36 @@ interface herbivorous{
 
 
 
-
+////  main class
 class animal_kingdom{
-}    
+}
+
+
+// clases of annimal groups
+    
 class mamalian exteds animalKingdom(
-      
+public function name ()      
 
 )
 
-class reptile{
+class reptile extends animal_kingdom{
     
 }
     
-class birds{
+class birds extends animal_kingdom{
     function fly(){}
     
 }
 
 
-class fish{
+class fish extends animal_kingdom{
     function dive(){}
         
 }   
 }
+
+// special animals
+
 
 
 ?>
